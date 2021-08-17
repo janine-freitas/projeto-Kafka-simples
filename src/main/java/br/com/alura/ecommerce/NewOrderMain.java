56c7e711dfa1c314757eb664b65f2be632740539
janine.freitas@nubank.com.br <janine.freitas@nubank.com.br>
2021-08-17 18:39:33 -0300

    Serializacao com GSON

--- src/main/java/br/com/alura/ecommerce/NewOrderMain.java
+++ src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -2,21 +2,27 @@ package br.com.alura.ecommerce;
 
 import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.math.BigDecimal;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
     public static void main(String [] args) throws ExecutionException, InterruptedException {
         //ENVIA MENSAGEM
-        try(var dispatcher = new KafkaDispatcher()){
-            for(var i = 0; i < 10; i++) {
-                var key = UUID.randomUUID().toString();
-                var value = key + ", 5000, 70000";
-                dispatcher.send("ECOMMERCE_NEW_ORDER", key, value);
+        try(var orderDispatcher = new KafkaDispatcher<Order>()){
+            try(var emailDispatcher = new KafkaDispatcher<String>()) {
+                for (var i = 0; i < 10; i++) {
+                    var userId = UUID.randomUUID().toString();
+                    var orderId = UUID.randomUUID().toString();
+                    var amount = new BigDecimal(Math.random() * 500 + 1);
+                    Order order = new Order(userId, orderId, amount);
+                    orderDispatcher.send("ECOMMERCE_NEW_ORDER", userId, order);
 
-                var email = "Thank you for your order. We are processing your order.";
-                var emailRecord = new ProducerRecord<String, String>("ECOMMERCE_SEND_EMAIL", key, email);
-                dispatcher.send("ECOMMERCE_SEND_EMAIL", key, value);
+                    var email = "Thank you for your order. We are processing your order.";
+                    var emailRecord = new ProducerRecord<String, String>("ECOMMERCE_SEND_EMAIL", userId, email);
+                    emailDispatcher.send("ECOMMERCE_SEND_EMAIL", userId, email);
+                }
             }
         }
     }
